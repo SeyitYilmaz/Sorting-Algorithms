072455c8c51f5c952283da8b08c69e5c1031b434
Seyit Yılmaz <30367777+SeyitYilmaz@users.noreply.github.com>
2019-10-30 13:03:14 +0300

    Initial commit

---
+++ Sorting Algorithms/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithims
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int eleman = 0;
+            int[] x = { 3, 8, 4, 9, 4 ,1};
+            int size = x.Length;
+
+
+            for (int i = 0; i < size; i++)
+            {
+                eleman = x[i];
+                for (int y = i + 1; y < size; y++)
+                {
+                    if (x[y] < x[i])
+                    {
+                        x[i] = x[y];
+                        x[y] = eleman;
+                    }
+
+                }
+
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(" " + x[i]);
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

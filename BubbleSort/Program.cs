92e098473e1917981072b61db4e8b2ad9a80692f
Seyit Yılmaz <30367777+SeyitYilmaz@users.noreply.github.com>
2019-11-01 09:35:42 +0300

    Bubble Sort Eklendi

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] dizi = { 2, 5, 7, 2, 3, 9 };
+            int sanalDepo;
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                bool siralimi = true;
+                for (int j = dizi.Length-1; j> i; j--)
+                {
+
+                    if (dizi[j-1]>dizi[j])
+                    {
+                        siralimi = false;
+                        sanalDepo = dizi[j - 1];
+                        dizi[j - 1] = dizi[j];
+                        dizi[j] = sanalDepo;
+                    }
+
+                }
+
+                if (siralimi)
+                {
+                    break;
+                }
+
+            }
+
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.Write(" " + dizi[i]);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}

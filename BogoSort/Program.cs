62502a098fc15254d772a83066131f4af8776ad2
Seyit Yılmaz <30367777+SeyitYilmaz@users.noreply.github.com>
2019-11-03 01:01:37 +0300

    Initial Commit

--- BogoSort/Program.cs
+++ BogoSort/Program.cs
@@ -10,6 +10,62 @@ namespace BogoSort
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>() { 2, 52, 21, 3,5,4 };
+
+            BogoSort(list);
+
+            Console.ReadKey();
+        }
+
+        static void BogoSort(List<int> list)
+        {
+            int iteration = 0;
+            while (!IsSorted(list))
+            {
+                PrintIteration(list, iteration);
+                list = Shuffle(list);
+                iteration++;
+            }
+
+            PrintIteration(list, iteration);
+
+        }
+
+        static void PrintIteration(List<int> list, int iteration)
+        {
+
+            foreach (var value in list)
+            {
+                Console.Write($"{value} ");
+            }
+            Console.WriteLine();
+        }
+        static bool IsSorted(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] > list[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static List<int> Shuffle(List<int> numbers)
+        {
+            Random r = new Random();
+           
+            for (int n = numbers.Count - 1; n > 0; --n)
+            {  
+                int k = r.Next(n + 1);  
+                int temp = numbers[n];
+
+                numbers[n] = numbers[k];
+                numbers[k] = temp;
+            }
+            return numbers;
         }
     }
 }
--- ShakerSort/Program.cs
+++ ShakerSort/Program.cs
@@ -10,6 +10,61 @@ namespace ShakerSort
     {
         static void Main(string[] args)
         {
+            int[] dizi = { 1, 5, 84, 12, 52, 47, 25 };
+
+            ShakerSort(dizi);
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.Write(" " + dizi[i]);
+            }
+            Console.ReadKey();
+
+        }
+
+        public static void ShakerSort(int[] dizi)
+        {
+            bool isSwapped = true;
+            int start = 0;
+            int end = dizi.Length;
+
+            while (isSwapped == true)
+            {
+
+
+                isSwapped = false;
+
+                for (int i = start; i < end - 1; ++i)
+                {
+                    if (dizi[i] > dizi[i + 1])
+                    {
+                        int temp = dizi[i];
+                        dizi[i] = dizi[i + 1];
+                        dizi[i + 1] = temp;
+                        isSwapped = true;
+                    }
+                }
+
+                if (isSwapped == false)
+                    break;
+
+                isSwapped = false;
+
+                end = end - 1;
+
+                for (int i = end - 1; i >= start; i--)
+                {
+                    if (dizi[i] > dizi[i + 1])
+                    {
+                        int temp = dizi[i];
+                        dizi[i] = dizi[i + 1];
+                        dizi[i + 1] = temp;
+                        isSwapped = true;
+                    }
+                }
+
+                start = start + 1;
+            }
         }
     }
 }
